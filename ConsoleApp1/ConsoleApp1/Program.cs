43c3112c04c4f4e3522e3631659f424ea1dc501c
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-03-10 11:00:15 -0300

    finaliza programa

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um número para realizarmos uma contagem regressiva à partir dele.");
+
+            int numero = Convert.ToInt32( Console.ReadLine());
+
+            int contagem = numero;
+
+            Console.WriteLine("Pressione uma tecla para proseguirmos");
+
+            Console.ReadKey();
+
+            Console.WriteLine("Preparar...");
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Apontar...");
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("FOGO!");
+
+            Console.WriteLine(numero);
+
+            Console.Beep();
+
+            Thread.Sleep(1000);
+
+            for (int i = 0; i < numero; i++)
+            {
+
+                int temp = contagem - 1;
+
+                contagem = temp;
+                
+                Console.WriteLine(temp);
+
+                Console.Beep();
+
+                Thread.Sleep(1000);
+
+            }
+
+            Console.WriteLine("ACABOU!");
+
+
+        }
+
+    }
+
+}
